ba77fdd4c9dee1839fed261f7572ba96d518aeec
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-08-17 00:36:21 +0300

    Arithmetics in PHP

---
+++ arithmetics.php
@@ -0,0 +1,35 @@
+<?php
+
+// Arithmetic operators
+// + - * / ** %
+
+// Increment/decrement operators
+// ++, --
+
+// Operator Precedence
+// ()
+// **
+// * / %
+// + -
+
+$x = 10;
+$y = 2;
+$z = null;
+
+$z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+// $z = $x ** $y; No. squared
+// $z = $x % $y;
+
+// echo $z;
+
+$counter = 0;
+
+// $counter = $counter + 1;
+// $counter++;
+$counter += 2;
+echo $counter;
+
+?>
